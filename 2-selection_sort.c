e4b278d5b3c70897bce33659a00970f66bb0f806
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-28 08:58:33 +0000

    3d task's solution

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/*
+ * selection_sort - Selection sort algorithm
+ * @array: an array to be sorted
+ * @size: the size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (array[i] > array[min_idx])
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
+}
