05b26f48c29149d6ee8027aacd273fcbb77b0d85
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-28 06:43:44 +0000

    1st task's solution

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - Bubble sorting algorithm
+ * @array: an array to be sorted
+ * @size: the size of tha array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
